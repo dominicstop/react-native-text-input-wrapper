909c0f023c3d5be78ef73f23ee93ef742a1f2b50
Dominic Go <18517029+dominicstop@users.noreply.github.com>
2024-02-23 00:46:16 +0800

    💫 Update: Ex - App Entry

--- example/App.tsx
+++ example/App.tsx
@@ -1,20 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as RNITextInputWrapper from 'react-native-text-input-wrapper';
+import { StyleSheet, TextInput, View } from "react-native";
+import { TextInputWrapperView } from "react-native-text-input-wrapper";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>{RNITextInputWrapper.hello()}</Text>
+    <View style={styles.rootContainer}>
+      <TextInputWrapperView
+        style={styles.textInputWrapper}
+        renderTextInput={() => (
+          <TextInput
+            style={styles.textInput}
+            placeholder="Write something..."
+          />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  textInputWrapper: {},
+  textInput: {},
 });
